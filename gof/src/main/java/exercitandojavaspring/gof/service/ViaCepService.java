07b25c9db19e8b1cdaf34cc37c10b33d41b3eebb
Brunocvale <brunocvale@gmail.com>
2022-08-05 22:02:31 -0300

    Initial commit

---
+++ gof/src/main/java/exercitandojavaspring/gof/service/ViaCepService.java
@@ -0,0 +1,14 @@
+package exercitandojavaspring.gof.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import exercitandojavaspring.gof.model.Endereco;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
